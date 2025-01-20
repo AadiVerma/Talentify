b1aeccbe8cd200fe5ddf4c1f231c5e239b5706bf
AadiVerma <adityakaplish11@gmail.com>
2025-01-20 22:27:00 +0530

    updates

---
+++ client/src/HomePage/Page1.jsx
@@ -0,0 +1,8 @@
+export default function Page1() {
+    return (
+        <div>
+            <h1 className="text-7xl flex justify-center ss4">Discover the world’s</h1>
+            <h2 className="text-7xl  flex justify-center ss4"> top talent</h2>
+        </div>
+    );
+}
